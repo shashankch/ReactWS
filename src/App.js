628441f432ca2b873f443ed34aae4a2410ccef6d
shashank singh chandel <shashakchandel@gmail.com>
2020-08-27 01:13:12 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -3,23 +3,32 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const name = 'shashank';
+  const isLoggedin = true;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <h1>dfjdlfd</h1>
+      <div className='App'>
+        <header className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
+          <p>
+            {/* Edit <code>src/App.js</code> and save to reload. */}
+            Hello world,My Name is {isLoggedin ? name : 'someUser'}{' '}
+            {new Date().toDateString()}
+            {!isLoggedin && <p>hello world</p>}
+            {isLoggedin && <p>hello {name}</p>}
+          </p>
+          <a
+            className='App-link'
+            href='https://reactjs.org'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    </React.Fragment>
   );
 }
 
